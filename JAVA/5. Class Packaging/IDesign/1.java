e6718de5dbf480086e747f27bbfd0ad7e2fb1c41
Aditya-Jagtap-10 <102784393+Aditya-Jagtap-10@users.noreply.github.com>
2022-04-11 11:53:17 +0530

    Added country file, output correct after submit.

--- JAVA/5. Class Packaging/IDesign/1.java
+++ JAVA/5. Class Packaging/IDesign/1.java
@@ -1,32 +1,53 @@
-// Main.java
+/****Main****/
 
-import java.util.Scanner; 
+import java.util.*;
+import java.util.Scanner.*;
 public class Main {
 
 	public static void main(String[] args){
-	  Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
         System.out.println("Enter the country name");
         String name = sc.nextLine();
         System.out.println("Enter the country code");
         String countryCode = sc.nextLine();
         System.out.println("Enter the isd code");
         String isdCode = sc.nextLine();
-        Country country = new Country(name,countryCode,isdCode);
-        System.out.println(String.format("Country Name:%s\nCountry Code:%s\nISD Code:%s",name,countryCode,isdCode));
+        Country c = new Country(name,countryCode,isdCode);
+        c.display();
         sc.close();
 	}
 
 }
 
+/****Country****/
 
-// Country.java
+import java.util.*;
 
 public class Country{
-    public String name,countryCode,isdCode;
+    public String name;
+    public String countryCode;
+    public String isdCode;
+  	Country(String name, String countryCode, String isdCode){
+		  this.name = name;
+		  this.countryCode = countryCode;
+		  this.isdCode = isdCode;
+	}
     
-    Country(String name, String countryCode, String isdCode) {
-        this.name = name;
-        this.countryCode = countryCode;
-        this.isdCode = isdCode;
+    public String getName(){
+        return this.name;
+    }
+	
+    public String getcountryCode(){
+        return this.countryCode;
+    }
+
+    public String getisdCode(){
+        return this.isdCode;
+    }
+
+    public void display(){
+        System.out.println("Country Name:"+name);
+        System.out.println("Country Code:"+countryCode);
+        System.out.println("ISD Code:"+isdCode);
     }
 }
